f0ed724ec507ad01862b104fc6b76f48866b0ba0
mari4l3 <mariaalejandras703@gmail.com>
2024-09-07 14:48:45 -0500

    Implementación inicial de la clase Cancion

---
+++ demo/src/main/java/Cancion.java
@@ -0,0 +1,84 @@
+public class Cancion {
+        
+private String titulo;
+private String artista;
+private int duracion; 
+private String genero;
+private String album;
+
+
+// Constructor por defecto
+public Cancion() {
+    this.titulo = "Desconocido";
+    this.artista = "Desconocido";
+    this.duracion = 0;
+    this.genero = "Desconocido";
+    this.album = "Desconocido";
+}
+    
+// Constructor con parametros. 
+public Cancion(String titulo, String artista, int duracion, String genero) {
+    this.titulo = titulo;
+    this.artista = artista;
+    this.duracion = duracion;
+}
+
+// Métodos getter y setter
+
+public String getTitulo() {
+    return titulo;
+}
+
+public void setTitulo(String titulo) {
+    this.titulo = titulo;
+}
+
+public String getArtista() {
+    return artista;
+}
+
+public void setArtista(String artista) {
+    this.artista = artista;
+}
+
+public int getDuracion() {
+    return duracion;
+}
+
+public void setDuracion(int duracion) {
+    this.duracion = duracion;
+}
+
+public String getGenero() {
+    return genero;
+}
+
+public void setGenero(String genero) {
+    this.genero = genero;
+}
+
+// Método para calcular la duración en minutos
+
+private int calcularDuracionMinutos() {
+    return duracion / SEGUNDOS_POR_MINUTO;
+}
+//constante estática "SEGUNDOS_POR_MINUTO":
+public static final int SEGUNDOS_POR_MINUTO = 60;
+
+// Método para mostrar la información
+public void mostrarInformacion() {
+    System.out.println("Título: " + titulo);
+    System.out.println("Artista: " + artista);
+    System.out.println("Duración: " + calcularDuracionMinutos() + " minutos");
+    System.out.println("Género: " + genero);
+    System.out.println("Álbum: " + album);
+}
+
+// Sobrecarga del método mostrarInformacion
+public void mostrarInformacion(String usuario) {
+    mostrarInformacion();
+    System.out.println("Escuchado por: " + usuario);
+}
+
+}
+
